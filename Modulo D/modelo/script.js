3aeba51d54d5d65021b08807bb75ddd2a74bb99d
Thiago R <thiagodaqui@gmail.com>
2019-12-26 10:55:19 -0300

    inserida fotos e pequena logica js

--- Modulo D/modelo/script.js
+++ Modulo D/modelo/script.js
@@ -1,1 +1,25 @@
-alert("ola")
+function carregar() {
+  let message = window.document.getElementById('msg')
+  let img = window.document.getElementById('imagem')
+  let data = new Date()
+  //let hora = data.getHours()
+  let hora = 11
+
+  message.innerHTML = `Agora são ${hora} horas.`
+
+  if (hora >= 0 && hora < 12){
+    //dia
+    img.src = "../dia.png"
+    document.body.style.background = '#FCF282'
+  } else if (hora >= 12 && hora < 18) {
+    //tarde
+    img.src = "../tarde.png"
+    document.body.style.background = '#F0A52F'
+  } else {
+    //noite
+    img.src = "../noite.png"
+    document.body.style.background = '#1B1A9A'
+  }
+  
+}
+
